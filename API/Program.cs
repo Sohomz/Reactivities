496455e56a64457c08b0a3c15c6092ec9616e057
Sohom Das <sohomd088@gmail.com>
2026-07-26 14:46:09 +0530

    section 12 ends with IDP end points

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,10 @@
 using API.Middleware;
 using Application.Activities.Commands;
 using Application.Activities.Queries;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,7 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddDbContext<Persistence.AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -24,18 +32,32 @@ builder.Services.AddCors(opt =>
 builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddTransient<ExceptionMiddleware>();
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors("CorsPolicy");
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:7268", "https://localhost:7268"));
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGroup("api").MapIdentityApi<User>();
 
 
 using var scope = app.Services.CreateScope();
@@ -44,12 +66,13 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<AppDbContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
 
     // Applies any pending migrations and creates the database if it doesn't exist
     await context.Database.MigrateAsync();
 
     // Seeds the data
-    await Persistence.DbInitializer.Initialize(context);
+    await Persistence.DbInitializer.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,9 +1,11 @@
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
 //options is a class that contains all the options for configuring appsettings the context
-public class AppDbContext (DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext (DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public DbSet<Domain.Activity> Activities {get; set;}
 }
